594d20c4cb2bb9c95b9af20aa65013e1d066d10c
FatcatEx <121566648+FatcatEx@users.noreply.github.com>
2024-11-17 15:10:35 +0800

    Add files via upload

--- samples/sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ samples/sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 LINE Corporation
+ * Copyright 2016 LINE Corporation
  *
  * LINE Corporation licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -16,47 +16,33 @@
 
 package com.example.bot.spring.echo;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.linecorp.bot.messaging.client.MessagingApiClient;
-import com.linecorp.bot.messaging.model.ReplyMessageRequest;
-import com.linecorp.bot.messaging.model.TextMessage;
-import com.linecorp.bot.spring.boot.handler.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.handler.annotation.LineMessageHandler;
-import com.linecorp.bot.webhook.model.Event;
-import com.linecorp.bot.webhook.model.MessageEvent;
-import com.linecorp.bot.webhook.model.TextMessageContent;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
     private final Logger log = LoggerFactory.getLogger(EchoApplication.class);
-    private final MessagingApiClient messagingApiClient;
 
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
 
-    public EchoApplication(MessagingApiClient messagingApiClient) {
-        this.messagingApiClient = messagingApiClient;
-    }
-
     @EventMapping
-    public void handleTextMessageEvent(MessageEvent event) {
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         log.info("event: " + event);
-        if (event.message() instanceof TextMessageContent) {
-            TextMessageContent message = (TextMessageContent) event.message();
-            final String originalMessageText = message.text();
-            messagingApiClient.replyMessage(new ReplyMessageRequest(
-                    event.replyToken(),
-                    List.of(new TextMessage(originalMessageText)),
-                    false));
-        }
+        final String originalMessageText = event.getMessage().getText();
+        return new TextMessage("测试成功。");
     }
 
     @EventMapping
